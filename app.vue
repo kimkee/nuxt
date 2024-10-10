acac12daae914dd289052f2a9793d44738c18318
KimKeeHyun <kimkee7@gmail.com>
2024-10-10 21:09:46 +0900

    init

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div class="kimkee">
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+      <p class="text-xl font-bold">dsfdgfsdf</p>
+    </div>
+    <NuxtWelcome />
+  </div>
+</template>
+<style>
+  .kimkee{
+    @apply bg-gray-600 
+  }
+</style>
