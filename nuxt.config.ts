84350894ec3c27a660eabead2cd37769f7e1bb4d
KimKeeHyun <kimkee7@gmail.com>
2024-10-12 17:59:01 +0900

    api: "modern-compiler" 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@vite-pwa/nuxt'
+    '@nuxtjs/tailwindcss'
   ],
   vite: {
     css: {
