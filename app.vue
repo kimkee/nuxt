e99d236497361e44766b0694706832b9d980db6c
KimKeeHyun <kimkee7@gmail.com>
2024-10-12 22:24:35 +0900

    dd

--- app.vue
+++ app.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="kimkee bg-black">
-    <div class="kimkee__h1">ddd</div>
     <NuxtRouteAnnouncer />
     <div class="flex flex-col  items-center justify-center p-16">
-      <h1 class="font-semibold text-4xl text-white">Nuxton!!</h1>
+      <h1 class="title font-semibold text-4xl text-white">Nuxton!!</h1>
       <p class="text-lg text-gray-500">Get started</p>
     </div>
 
@@ -12,12 +11,16 @@
     
   </div>
 </template>
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .kimkee {
-  background: #0d0;
-  &__h1{ 
-    font-weight: 700;
+  @apply bg-slate-900;
 
+  h1 { 
+    @apply text-yellow-500;
+
+  }
+  &:hover {
+    @apply text-white
   }
 }
 </style>
