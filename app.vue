f64083eb850dd96d16fcdbd2202f61880dcc0817
KimKeeHyun <kimkee7@gmail.com>
2024-10-12 17:50:16 +0900

    api: "modern-compiler" 2

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="kimkee bg-black">
     <NuxtRouteAnnouncer />
     <div class="flex flex-col  items-center justify-center p-16">
-      <h1 class="font-semibold text-4xl text-white">Nuxton!</h1>
+      <h1 class="font-semibold text-4xl text-white">Nuxton!!</h1>
       <p class="text-lg text-gray-500">Get started</p>
     </div>
 
