3ab782ddd085a3d0fd75326729e331a37ad98823
KimKeeHyun <kimkee7@gmail.com>
2024-10-12 14:07:46 +0900

    pwa setting

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="kimkee bg-black">
     <NuxtRouteAnnouncer />
-    <div class="flex flex-col  items-center justify-center p-10">
-      <h1 class="font-semibold text-4xl text-white">Welcome to Nuxt!</h1>
-      <p class="text-lg text-gray-500">Get started GO</p>
+    <div class="flex flex-col  items-center justify-center p-16">
+      <h1 class="font-semibold text-4xl text-white">Nuxton!</h1>
+      <p class="text-lg text-gray-500">Get started</p>
     </div>
 
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,37 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt'
+  ],
+  pwa: {
+    manifest: {
+      name: 'Nuxton',
+      short_name: 'NuxtPWA',
+      description: 'This is my awesome Nuxt PWA!',
+      theme_color: '#ffffff',
+      background_color: '#000000',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/img/icon_app.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: 'https://nuxtom.pages.dev/.*',
+          handler: 'NetworkFirst',
+          method: 'GET'
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true
+    }
+  }
 })
