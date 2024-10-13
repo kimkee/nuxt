e5b248211c36d31f76a063471ffda54145b1ee9f
KimKeeHyun <kimkee7@gmail.com>
2024-10-13 12:17:03 +0900

    icon

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="kimkee bg-black">
+  <div class="kimkee">
     <NuxtRouteAnnouncer />
     <div class="flex flex-col  items-center justify-center p-16">
+      <div class="text-center">
+        <img class="w-20 h-20" src="~/assets/img/icon.png" alt="dd">
+      </div>
       <h1 class="title font-semibold text-4xl text-white">Nuxton!!</h1>
-      <p class="text-lg text-gray-500">Get started</p>
+      <p class="text-lg text-gray-500 leading-10">Get started</p>
     </div>
 
 
@@ -13,9 +16,9 @@
 </template>
 <style lang="css" scoped>
 .kimkee {
-  @apply bg-slate-900;
+  @apply bg-gray-900;
   h1 { 
-    @apply text-white;
+    @apply text-white cursor-pointer selection:not-sr-only;
     &:hover {
       @apply text-yellow-500;
     }
