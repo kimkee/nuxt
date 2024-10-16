72c4f0bbba8ae795a6a836d895c912dd45ecba51
KimKeeHyun <kimkee7@gmail.com>
2024-10-16 13:14:04 +0900

    manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'manifest', href: '/manifest.json' }
+      ]
+    }
+  },
   modules: ['@nuxtjs/tailwindcss', '@vesp/nuxt-fontawesome', '@vite-pwa/nuxt'],
   nitro: {
     prerender: {
