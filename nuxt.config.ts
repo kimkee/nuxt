e1f71ef81bdcc763dacf7ae058c1ef6510a80069
KimKeeHyun <kimkee7@gmail.com>
2024-10-16 13:47:25 +0900

    icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       ]
     }
   },
-  modules: ['@nuxtjs/tailwindcss', '@vesp/nuxt-fontawesome', '@vite-pwa/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@vesp/nuxt-fontawesome'],
   nitro: {
     prerender: {
       autoSubfolderIndex: false
@@ -31,44 +31,6 @@ export default defineNuxtConfig({
                 api: "modern-compiler"
             }
         }
-    }
-  },
-  pwa: {
-    manifest: {
-      "name": "VUE FIREBASE",
-      "short_name": "VUE FIREBASE",
-      "icons": [
-          {
-            "src": "./img/favicon.ico",
-            "sizes": "32x32",
-            "type": "image/x-icon"
-          },
-          {
-            "src": "./img/icon_app.png",
-            "type": "image/png",
-            "sizes": "512x512"
-          }
-        ],
-      "start_url": "./",
-      "display": "standalone",
-      "background_color": "#1d2736",
-      "theme_color": "#1d2736"
-  },
-    workbox: {
-      /* Workbox 설정 예시 */
-      runtimeCaching: [
-        {
-          urlPattern: '/*',
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'my-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 300
-            }
-          }
-        }
-      ]
     }
   }
 })
