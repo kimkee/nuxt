30d7fbb98fc9df1f02858d3197e213aaa92621c4
KimKeeHyun <kimkee7@gmail.com>
2024-10-16 20:53:23 +0900

    useSeoMeta

--- app.vue
+++ app.vue
@@ -14,10 +14,12 @@ useSeoMeta({
 <template>
   <div class="kimkee p-2">
     <NuxtRouteAnnouncer />
-    <header class="flex justify-start px-3 py-1">
-      <div class="text-center py-1">
-        <img class="w-20 mb-1" src="//nuxt.com/assets/design-kit/logo-green-white.svg" alt="dd">
-      </div>
+    <header class="flex justify-start px-3 py-1 h-14">
+      <a href="javascript:;" class="text-center py-1 inline-flex items-center">
+        <!-- <img class="w-20 mb-1" src="//nuxt.com/assets/design-kit/logo-green-white.svg" alt="dd"> -->
+        <img class="w-8 mr-1" src="~/assets/img/logo.svg" alt="dd">
+        <span class="text-white font-bold text-2xl">Nuxt</span>
+      </a>
     </header>
     <div class="flex flex-col  items-center justify-center p-16">
       <div class="flex justify-center grid-flow-col auto-cols-fr gap-10 w-full text-center text-white text-2xl mt-3">
@@ -26,7 +28,7 @@ useSeoMeta({
         <i class="min-w-8"><font-awesome :icon="['fas', 'magnifying-glass']" /></i>
         <i class="min-w-8"><font-awesome :icon="['far', 'user']" /></i>
       </div>
-      <h1 class="title font-semibold text-4xl text-white mt-6">Nuxton!!</h1>
+      <h1 class="title font-semibold text-4xl text-white mt-6">NuxTon</h1>
       <p class="text-lg text-gray-500 leading-10">Get started</p>
     </div>
    
