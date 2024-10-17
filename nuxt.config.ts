556e1028cccccd11f102e2dc0bd09fb3962d1afe
KimKeeHyun <kimkee7@gmail.com>
2024-10-17 13:43:31 +0900

    width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      viewport: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no',
       link: [
         { rel: 'manifest', href: '/manifest.json' }
       ]
