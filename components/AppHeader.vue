4e5eaaa88d6aa87bc5ea1fa3aab92eaa38179dc8
KimKeeHyun <kimkee7@gmail.com>
2024-10-18 13:18:31 +0900

    dark

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="flex flex-col justify-center min-h-14  safe-top-pd box-content drop-shadow-sm">
       
-      <div class="flex items-center safe-top-pd px-6 min-h-14 border-b bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 fixed top-0 left-0 right-0">
+      <div class="flex items-center safe-top-pd px-5 min-h-14 border-b bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 fixed top-0 left-0 right-0">
         
         <div class="flex items-center justify-between w-full min-h-14">
           <NuxtLink to="/" class="text-center py-1 -ml-2 inline-flex items-center">
--- components/Darkmode.vue
+++ components/Darkmode.vue
@@ -1,10 +1,10 @@
 <template>
   <button 
-    class="w-8 h-8 rounded-full bg-gray-400 dark:bg-gray-600 inline-flex justify-center items-center drop-shadow-sm" 
+    class="w-8 h-8 rounded-full bg-gray-300 dark:bg-gray-600 inline-flex justify-center items-center drop-shadow-sm" 
     @click="toggleDarkMode"
   >
     <span class="w-6 h-6 rounded-full bg-white dark:bg-gray-900 text-yellow-400 inline-flex justify-center items-center text-sm">
-      <i>
+      <i class="leading-3">
         <font-awesome v-if="isDarkMode" :icon="['fas', 'moon']" />
         <font-awesome  v-else :icon="['fas', 'sun']" />
       </i>  
@@ -14,17 +14,42 @@
 </template>
 
 <script setup>
+// const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
+// const locDark  = JSON.parse(localStorage.getItem('darkmode'));
 
+// const initDark = ()=> locDark === null ? setDark( darkMode.matches ) : setDark( locDark ) ;
+const togDark  = ()=> setDark( !htmlCls.contains('dark') );
+const setDark  = (isDark)=> {
+    chkTogs.forEach( tog => tog.checked = isDark );
+    htmlCls.toggle('dark', isDark);
+    localStorage.setItem('darkmode',isDark);
+}
 const isDarkMode = ref(false)
 
 const toggleDarkMode = () => {
   isDarkMode.value = !isDarkMode.value
   if (isDarkMode.value) {
     document.documentElement.classList.add('dark')
+    localStorage.setItem('darkmode',true);
   } else {
     document.documentElement.classList.remove('dark')
+    localStorage.setItem('darkmode',false);
   }
 }
+// document.addEventListener('DOMContentLoaded', initDark);
+// darkMode.addEventListener('change', initDark);
+onMounted(()=>{
+  const locDark  = JSON.parse(localStorage.getItem('darkmode'));
+  console.log(locDark);
+  isDarkMode.value = locDark
+  if (isDarkMode.value) {
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('darkmode',true);
+  } else {
+    document.documentElement.classList.remove('dark')
+    localStorage.setItem('darkmode',false);
+  }
+})
 </script>
 
 <style>
