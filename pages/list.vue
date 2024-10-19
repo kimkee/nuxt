b70eb873cead997a3508bc4fa3f25a9af647c832
KimKeeHyun <kimkee7@gmail.com>
2024-10-19 15:37:59 +0900

    css

--- pages/list.vue
+++ pages/list.vue
@@ -15,5 +15,20 @@
     <p>
       <NuxtLink to="/">Home</NuxtLink>
     </p>
+    <h1 class="font-['Roboto']        font-extrabold text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
+    <h1 class="font-['Roboto']        font-bold text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
+    <h1 class="font-['Roboto']        font-medium text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
+    <h1 class="font-['Roboto']        font-normal text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
+    <h1 class="font-['Roboto']        font-light text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
+    <h1 class="font-['Roboto']        font-extralight text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extrabold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-bold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-medium text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-normal text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-light text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extralight text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <p>
+      <NuxtLink to="/">Home</NuxtLink>
+    </p>
   </main>
 </template>
