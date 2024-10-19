78f5ba0e445cb2377e2dbf8d22a13fea5b756a54
KimKeeHyun <kimkee7@gmail.com>
2024-10-19 19:44:28 +0900

    bg-white/80 dark:bg-gray-800/80 backdrop-blur-sm

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="flex flex-col justify-center min-h-14 safe-top-pd box-content">
       
-      <div class="flex items-center justify-between safe-top-pd px-5 min-h-14 border-b bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 fixed top-0 left-0 right-0 drop-shadow-sm">
+      <div class="flex items-center justify-between safe-top-pd px-5 min-h-14 box-content border-b bg-white/80 dark:bg-gray-800/80 backdrop-blur-sm border-gray-200 dark:border-gray-800 fixed top-0 left-0 right-0 drop-shadow-sm">
         
-        <div class="flex items-center justify-between w-full min-h-14">
+        
           <NuxtLink to="/" class="text-center py-1 -ml-2 inline-flex items-center">
             <img class="w-8 mr-1" src="~/assets/img/logo.svg" alt="dd">
             <span class="text-gray-600 dark:text-white font-bold text-2xl">Nuxton</span>
@@ -12,7 +12,7 @@
           <!-- <button class="w-8 h-8">
             <i class="min-w-8 text-white text-xl"><font-awesome :icon="['far', 'user']" /></i>
           </button> -->
-        </div>
+        
       </div>
     </header>
 </template>
