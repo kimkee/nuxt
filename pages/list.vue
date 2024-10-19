a403905131c1a278114f0664ac88c86431e8ef5f
KimKeeHyun <kimkee7@gmail.com>
2024-10-19 15:48:38 +0900

    css

--- pages/list.vue
+++ pages/list.vue
@@ -1,32 +1,30 @@
 <template>
   <main class="container flex-1">
-    <h1 class="font-['Roboto']        font-extrabold text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-bold text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-medium text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-normal text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-light text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-extralight text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-extrabold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-bold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-medium text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-normal text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-light text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-extralight text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <p>
-      <NuxtLink to="/">Home</NuxtLink>
-    </p>
-    <h1 class="font-['Roboto']        font-extrabold text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-bold text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-medium text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-normal text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-light text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Roboto']        font-extralight text-xl mt-6">NuxT - 넉스트 [Roboto]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-extrabold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-bold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-medium text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-normal text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-light text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
-    <h1 class="font-['Noto_Sans_KR']  font-extralight text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR]</h1>
+    <h1 class="font-['Roboto']        font-extrabold text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-bold text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-medium text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-normal text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-light text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-extralight text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extrabold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-bold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-medium text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-normal text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-light text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extralight text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+
+    <h1 class="font-['Roboto']        font-extrabold text-xl mt-6">NuxT - 넉스트 [Roboto]NuxT - 0123456</h1>
+    <h1 class="font-['Roboto']        font-bold text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-medium text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-normal text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-light text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Roboto']        font-extralight text-xl mt-6">NuxT - 넉스트 [Roboto] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extrabold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-bold text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-medium text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-normal text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-light text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extralight text-xl mt-6">NuxT - 넉스트 [Noto_Sans_KR] - 0123456</h1>
     <p>
       <NuxtLink to="/">Home</NuxtLink>
     </p>
