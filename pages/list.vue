9a34348eb38a7516835d8783d3b44586c0548603
KimKeeHyun <kimkee7@gmail.com>
2024-10-19 18:27:31 +0900

    SpoqaHanSans

--- pages/list.vue
+++ pages/list.vue
@@ -1,5 +1,10 @@
 <template>
   <main class="container flex-1 space-y-4">
+    
+    <h1 class="font-['Roboto']        font-normal text-xl">NuxT - 넉스트률걁 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-normal text-xl">NuxT - 넉스트률걁 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-normal text-xl">NuxT - 넉스트률걁 - 0123456</h1>
+    <div class="border-t border-gray-300 mt-4"></div>
     <h1 class="font-['Roboto']        font-extrabold ">[Roboto] NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Roboto']        font-bold ">[Roboto] NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Roboto']        font-medium ">[Roboto] NuxT - 넉스트 - 0123456</h1>
@@ -7,19 +12,19 @@
     <h1 class="font-['Roboto']        font-light ">[Roboto] NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Roboto']        font-extralight ">[Roboto] NuxT - 넉스트 - 0123456</h1>
     <div class="border-t border-gray-300 mt-4"></div>
-    <h1 class="font-['NotoSansKR']  font-extrabold ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-bold ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-medium ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-normal ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-light ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-extralight ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extrabold ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-bold ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-medium ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-normal ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-light ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extralight ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
     <div class="border-t border-gray-300 mt-4"></div>
-    <h1 class="font-['SpoqaHanSansNeo']  font-extrabold ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-bold ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-medium ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-normal ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-light ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-extralight ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-extrabold ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-bold ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-medium ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-normal ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-light ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-extralight ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
     <div class="border-t border-gray-300 mt-4"></div>
     <h1 class="font-['Roboto']        font-extrabold ">[Roboto]NuxT NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Roboto']        font-bold ">[Roboto] NuxT - 넉스트 - 0123456</h1>
@@ -28,19 +33,19 @@
     <h1 class="font-['Roboto']        font-light ">[Roboto] NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Roboto']        font-extralight ">[Roboto] NuxT - 넉스트 - 0123456</h1>
     <div class="border-t border-gray-300 mt-4"></div>
-    <h1 class="font-['NotoSansKR']  font-extrabold ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-bold ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-medium ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-normal ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-light ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['NotoSansKR']  font-extralight ">[NotoSansKR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extrabold ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-bold ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-medium ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-normal ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-light ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Noto_Sans_KR']  font-extralight ">[Noto_Sans_KR] NuxT - 넉스트 - 0123456</h1>
     <div class="border-t border-gray-300 mt-4"></div>
-    <h1 class="font-['SpoqaHanSansNeo']  font-extrabold ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-bold ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-medium ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-normal ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-light ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
-    <h1 class="font-['SpoqaHanSansNeo']  font-extralight ">[SpoqaHanSansNeo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-extrabold ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-bold ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-medium ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-normal ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-light ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    <h1 class="font-['Spoqa_Han_Sans_Neo']  font-extralight ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
     <p>
       <NuxtLink to="/">Home</NuxtLink>
     </p>
