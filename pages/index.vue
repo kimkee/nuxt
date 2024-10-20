d11a87934bdb53c024a25034b5322c15c997888c
KimKeeHyun <kimkee7@gmail.com>
2024-10-20 22:00:20 +0900

    버튼

--- pages/index.vue
+++ pages/index.vue
@@ -12,22 +12,24 @@ useSeoMeta({
 <template>
   <main class="container flex-1 items-center justify-center flex flex-col">
     <div class="flex flex-col  items-center justify-center py-6 text-center">
-      <h1 class="text-5xl font-bold tracking-tight text-gray-900 dark:text-white"> 
-      The Intuitive<br>
-      <span class="text-primary block lg:inline-block">Vue Framework</span>
-    </h1>
-    <p class="mt-6 text-lg max-w-xl mx-auto text-gray-500 dark:text-gray-400 text-center">
-      Nuxt is a hybrid Vue framework that gives you the best of both server-side and client-side rendering.
-    </p>
+      <h1 class="text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
+        The Intuitive<br>
+        <span class="text-primary block lg:inline-block">Vue Framework</span>
+      </h1>
+      <p class="mt-6 text-lg max-w-xl mx-auto text-gray-500 dark:text-gray-400 text-center">
+        Nuxt is a hybrid Vue framework that gives you the best of both server-side and client-side rendering.
+      </p>
 
-      <p class="text-lg text-gray-500 leading-10 mt-6"><GithubButton /></p>
+      <p class="text-lg text-gray-500 leading-10 mt-6">
+        <GithubButton />
+      </p>
     </div>
   </main>
 </template>
 
 <style lang="css" scoped>
 .container {
-  h1 { 
+  h1 {
     &:hover {
       @apply text-yellow-500;
     }
--- pages/list.vue
+++ pages/list.vue
@@ -46,8 +46,10 @@
     <h1 class="font-['Spoqa_Han_Sans_Neo']  font-normal ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Spoqa_Han_Sans_Neo']  font-light ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
     <h1 class="font-['Spoqa_Han_Sans_Neo']  font-extralight ">[Spoqa_Han_Sans_Neo] NuxT - 넉스트 - 0123456</h1>
+    
     <p>
-      <NuxtLink to="/">Home</NuxtLink>
-    </p>
+        <NuxtLink to="/" class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm text-white dark:text-gray-900 bg-gray-900 hover:bg-gray-800 disabled:bg-gray-900 aria-disabled:bg-gray-900 dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-white dark:aria-disabled:bg-white focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
+          href="/docs/guide/going-further/modules"><!----><span>홈으로</span><!----></NuxtLink>
+      </p>
   </main>
 </template>
