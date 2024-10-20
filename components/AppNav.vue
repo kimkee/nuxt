aca7fe899bd1e70a844d61142ceac9b001d984a3
KimKeeHyun <kimkee7@gmail.com>
2024-10-20 09:31:59 +0900

    css

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,16 +1,16 @@
 <template>
     <nav class="nav min-h-16 safe-bottom-pd box-content">
       <div class="flex min-h-16  px-6 gap-0 w-full text-center border-t border-gray-200 dark:border-gray-800 bg-white/70 dark:bg-gray-800/80 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 right-0">
-        <NuxtLink to="/" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd box-content">
           <i><font-awesome :icon="['fas', 'house']" /></i><div class="text-xs">Home</div>
         </NuxtLink>
-        <NuxtLink to="/about" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/about" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd box-content">
           <i><font-awesome :icon="['fas', 'box']" /></i><div class="text-xs">About</div>
         </NuxtLink>
-        <NuxtLink to="/list" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/list" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd box-content">
           <i><font-awesome :icon="['fas', 'bars']" /></i><div class="text-xs">List</div>
         </NuxtLink>
-        <NuxtLink to="/404" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/404" class="bt w-full py-2 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd box-content">
           <i><font-awesome :icon="['fas', 'user']" /></i><div class="text-xs"> Mypage</div>
         </NuxtLink>
       </div> 
