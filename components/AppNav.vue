73d83e0f64fa2d3438066f5e78a1c57b0cb3394c
KimKeeHyun <kimkee7@gmail.com>
2024-10-20 10:03:15 +0900

    css

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,16 +1,16 @@
 <template>
     <nav class="min-h-16 safe-bottom-pd box-content">
-      <div class="flex min-h-16 pt-2 pb-2 px-6 gap-4 w-full text-center border-t border-gray-200 dark:border-gray-800 bg-white/70 dark:bg-gray-800/80 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 right-0">
-        <NuxtLink to="/" class="w-1/4 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+      <div class="flex min-h-16 pt-1 pb-2 px-6 gap-4 w-full text-center border-t border-gray-200 dark:border-gray-800 bg-white/70 dark:bg-gray-800/80 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 right-0">
+        <NuxtLink to="/" class="w-1/4 pt-1 min-h-12 flex flex-col justify-start gap-1 safe-bottom-pd">
           <i><font-awesome :icon="['fas', 'house']" /></i><div class="text-xs">Home</div>
         </NuxtLink>
-        <NuxtLink to="/about" class="w-1/4 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/about" class="w-1/4 pt-1 min-h-12 flex flex-col justify-start gap-1 safe-bottom-pd">
           <i><font-awesome :icon="['fas', 'box']" /></i><div class="text-xs">About</div>
         </NuxtLink>
-        <NuxtLink to="/list" class="w-1/4 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/list" class="w-1/4 pt-1 min-h-12 flex flex-col justify-start gap-1 safe-bottom-pd">
           <i><font-awesome :icon="['fas', 'bars']" /></i><div class="text-xs">List</div>
         </NuxtLink>
-        <NuxtLink to="/404" class="w-1/4 min-h-11 flex flex-col justify-start gap-1 safe-bottom-pd">
+        <NuxtLink to="/404" class="w-1/4 pt-1 min-h-12 flex flex-col justify-start gap-1 safe-bottom-pd">
           <i><font-awesome :icon="['fas', 'user']" /></i><div class="text-xs"> Mypage</div>
         </NuxtLink>
       </div>
@@ -20,7 +20,7 @@
 .dark .active-link{@apply text-green-400;}
 .active-link{@apply text-green-600;}
 a:active{
-  -webkit-tap-highlight-color: transparent;
+  /* -webkit-tap-highlight-color: transparent; */
   @apply  scale-90 transition duration-300
 }
 </style>
