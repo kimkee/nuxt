de253d3bcc3b5c648912716d6dda92bb48e26cf6
KimKeeHyun <kimkee7@gmail.com>
2024-10-21 12:30:16 +0900

    버튼

--- pages/list.vue
+++ pages/list.vue
@@ -49,7 +49,7 @@
     
     <p>
         <NuxtLink to="/" class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 px-2.5 py-1.5 shadow-sm text-white dark:text-gray-900 bg-gray-900 hover:bg-gray-800 disabled:bg-gray-900 aria-disabled:bg-gray-900 dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-white dark:aria-disabled:bg-white focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
-          href="/docs/guide/going-further/modules"><!----><span>홈으로</span><!----></NuxtLink>
+          href="/docs/guide/going-further/modules"><span>홈으로</span></NuxtLink>
       </p>
   </main>
 </template>
