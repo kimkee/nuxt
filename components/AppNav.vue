b5d4d3bbc31f8b761cdeb06d49da1bf6526f0946
KimKeeHyun <kimkee7@gmail.com>
2024-10-25 16:23:38 +0900

    pre

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,31 +1,36 @@
 <template>
-    <nav class="min-h-16 safe-bottom-pd box-content">
-      <div class="min-h-16 safe-bottom-pd box-content  border-t border-gray-200 dark:border-gray-700 bg-white/70 dark:bg-gray-800/80 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 left-0 right-0">
-        <div class="flex text-center pt-2 px-5">
-          <NuxtLink to="/" class="w-full h-12 flex flex-col justify-start gap-1">
-            <i><font-awesome :icon="['fas', 'house']" /></i><div class="text-xs">Home</div>
-          </NuxtLink>
-          <NuxtLink to="/about" class="w-full h-12 flex flex-col justify-start gap-1">
-            <i><font-awesome :icon="['fas', 'box']" /></i><div class="text-xs">About</div>
-          </NuxtLink>
-          <NuxtLink to="/list" class="w-full h-12 flex flex-col justify-start gap-1">
-            <i><font-awesome :icon="['fas', 'bars']" /></i><div class="text-xs">List</div>
-          </NuxtLink>
-          <NuxtLink to="/login" class="w-full h-12 flex flex-col justify-start gap-1">
-            <i><font-awesome :icon="['fas', 'user']" /></i><div class="text-xs">MY</div>
-          </NuxtLink>
-        </div>
+  <nav class="min-h-16 safe-bottom-pd box-content">
+    <div class="min-h-16 safe-bottom-pd box-content border-t border-gray-200 dark:border-gray-700 bg-white/70 dark:bg-gray-800/80 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 left-0 right-0">
+      <div class="flex text-center pt-2 px-5">
+        <NuxtLink to="/" class="w-full h-12 flex flex-col justify-start gap-1">
+          <i><font-awesome :icon="['fas', 'house']" /></i>
+          <div class="text-xs">Home</div>
+        </NuxtLink>
+        <NuxtLink to="/about" class="w-full h-12 flex flex-col justify-start gap-1">
+          <i><font-awesome :icon="['fas', 'box']" /></i>
+          <div class="text-xs">About</div>
+        </NuxtLink>
+        <NuxtLink to="/list" class="w-full h-12 flex flex-col justify-start gap-1">
+          <i><font-awesome :icon="['fas', 'bars']" /></i>
+          <div class="text-xs">List</div>
+        </NuxtLink>
+        <NuxtLink to="/login" class="w-full h-12 flex flex-col justify-start gap-1">
+          <i><font-awesome :icon="['fas', 'user']" /></i>
+          <div class="text-xs">MY</div>
+        </NuxtLink>
       </div>
-    </nav>
-    
-
-   
+    </div>
+  </nav>
 </template>
 <style scoped>
-.dark .active-link{@apply text-green-400;}
-.active-link{@apply text-green-600;}
-a:active{
+.dark .active-link {
+  @apply text-green-400;
+}
+.active-link {
+  @apply text-green-600;
+}
+a:active {
   /* -webkit-tap-highlight-color: transparent; */
-  @apply  scale-90 transition duration-300
+  @apply scale-90 transition duration-300;
 }
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,34 +1,33 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
   <main class="container flex-1 items-center justify-center flex flex-col">
-
     <div class="flex flex-col max-w-[20rem] w-full justify-center items-center py-6 -mt-12">
-      <div class="mb-1"><img class="w-16" src="~/assets/img/logo.svg" alt="dd"></div>
+      <div class="mb-1">
+        <img class="w-16" src="~/assets/img/logo.svg" alt="dd" />
+      </div>
       <div class="my-4 text-center mb-4 relative before:absolute before:left-0 before:right-0 before:border-t before:border-gray-500/30 before:top-1/2 before:z-0 w-full">
-        <em class="text-green-600 dark:text-green-400 relative px-2 z-1 bg-white dark:bg-[#0d111b]">
-          로그인 {{ $ui.add(1, 2) }}
-        </em>
+        <p class="text-green-600 dark:text-green-400 relative px-2 z-1 bg-white dark:bg-[#0d111b]">로그인 {{ $ui.add(1, 2) }}</p>
       </div>
       <div class="grid grid-cols-2 gap-2 w-full">
-        
         <button @click="$ui.greet('Github')" type="button" class="btn btn-lg">
-          <i><font-awesome :icon="['fab', 'github']" /></i><em>Github </em>
+          <i><font-awesome :icon="['fab', 'github']" /></i>
+          <em>Github</em>
         </button>
         <button type="button" class="btn btn-lg">
-          <i><font-awesome :icon="['fab', 'google']" /></i><em>Google </em>
+          <i><font-awesome :icon="['fab', 'google']" /></i>
+          <em>Google</em>
         </button>
         <button type="button" class="btn btn-lg">
-          <i><font-awesome :icon="['fab', 'kickstarter-k']" /></i><em>Kakao </em>
+          <i><font-awesome :icon="['fab', 'kickstarter-k']" /></i>
+          <em>Kakao</em>
         </button>
         <button type="button" class="btn btn-lg">
-          <i><font-awesome :icon="['fab', 'twitter']" /></i><em>Twitter </em>
+          <i><font-awesome :icon="['fab', 'twitter']" /></i>
+          <em>Twitter</em>
         </button>
       </div>
     </div>
   </main>
 </template>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
