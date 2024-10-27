7220e85b99237602cfaab22e14147d360c104e16
KimKeeHyun <kimkee7@gmail.com>
2024-10-27 16:44:26 +0900

    유저정보

--- pages/user.vue
+++ pages/user.vue
@@ -1,18 +1,30 @@
 <script setup>
-import { useRouter } from 'vue-router';
+// import { useRouter } from 'vue-router';
+// import { useSupabaseClient, useSupabaseUser } from '@supabase/auth-helpers-nuxt';
 const supabase = useSupabaseClient()
 const router = useRouter();
-
+const user = useSupabaseUser(); 
+const userInfo = ref(null)
+userInfo.value = user.value
 const signOut = async () => {
   const { error } = await supabase.auth.signOut();
   if (error) console.log(error);
-  router.push('/about');
+  router.push('/');
 }
+console.log(userInfo);
+
+
 </script>
 <template>
   <main class="container flex-1 items-center justify-center flex flex-col">
     <div class="flex flex-col items-center gap-6">
-      <h1 class="text-4xl">Profile</h1>
+      <div> <img :src="userInfo.user_metadata.avatar_url" alt="" class="h-20 rounded-full"> </div>
+      
+      <div class="text-left">
+        <p>메일 : {{ userInfo.user_metadata.email }}</p>
+        <p>이름 : {{ userInfo.user_metadata.name }}</p>
+        <p>가입 : {{ userInfo.created_at }}</p>
+      </div>
       <div class="mt-2">
         <button class="btn" @click="signOut">로그아웃</button>
       </div>
