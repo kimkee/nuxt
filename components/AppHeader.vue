a97a2985a2ea4d0896fbf8c9240bbd4ee9e5b9e4
KimKeeHyun <kimkee7@gmail.com>
2024-10-29 11:29:54 +0900

    cdd

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,11 +2,11 @@
   <header class="flex flex-col justify-center min-h-14 safe-top-pd box-content">
     <div class="
       flex items-center justify-between safe-top-pd px-5 min-h-14 box-content 
-      bg-gradient-to-t from-white/20 to-white 
+      bg-gradient-to-t from-white/80 to-white 
       dark:bg-gradient-to-t dark:from-gray-800/80 dark:to-[#1f2937] 
       backdrop-blur-sm fixed top-0 left-0 right-0 z-50
       after:h-[1px] after:absolute after:left-0 after:bottom-0 after:bg-gray-600/5
-      after:w-full 
+      after:w-full
       "
       
     >
--- components/AppNav.vue
+++ components/AppNav.vue
@@ -7,7 +7,7 @@ console.log(userInfo.value);
 </script>
 <template>
   <nav class="min-h-16 safe-bottom-pd box-content">
-    <div class="min-h-16 safe-bottom-pd box-content border-t border-gray-200 dark:border-gray-700 bg-white/70 dark:bg-gray-800/80 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 left-0 right-0">
+    <div class="min-h-16 safe-bottom-pd box-content border-t border-gray-200 dark:border-gray-700 bg-white/80 dark:bg-gray-800/90 backdrop-blur-sm text-gray-600 dark:text-white fixed bottom-0 left-0 right-0">
       <div class="flex text-center pt-2 px-5">
         <NuxtLink to="/" class="w-full h-12 flex flex-col justify-start gap-1">
           <i><font-awesome :icon="['fas', 'house']" /></i>
