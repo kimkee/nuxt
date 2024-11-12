b48ae037b59ddcbed697a0443979d7e18fe697aa
KimKeeHyun <kimkee7@gmail.com>
2024-11-12 13:17:06 +0900

    server/api    test

---
+++ server/api/chat.js
@@ -0,0 +1,27 @@
+import { defineEventHandler, getQuery } from 'h3';
+import { createClient } from '@supabase/supabase-js';
+
+// Supabase 설정
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default defineEventHandler(async (event) => {
+  try {
+    const { table } = getQuery(event); // 쿼리 파라미터에서 테이블 이름 가져오기
+    const { data, error } = await supabase
+      .from(table)
+      .select('*')
+      .order('created_at', { ascending: true });
+
+    if (error) {
+      console.error('Error fetching users:', error);
+      return { error: 'Error fetching users' };
+    }
+    
+    return data;
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    return { error: 'Unexpected error' };
+  }
+});
