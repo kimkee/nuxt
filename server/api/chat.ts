9da08b654a9954de61e327b50290f2edc5fe47f4
KimKeeHyun <kimkee7@gmail.com>
2024-11-15 09:14:25 +0900

    server/api

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -7,25 +7,21 @@ const supabaseKey = process.env.SUPABASE_KEY ?? '';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default defineEventHandler(async (event) => {
-  try {
-    const { table  } = getQuery(event); // 쿼리 파라미터에서 테이블 이름 가져오기
-    // 테이블 이름이 유효한지 확인
-    if (!table || typeof table !== 'string') {
-      throw new Error('Invalid table name');
-    }
-    const { data, error } = await supabase
-      .from(table)
-      .select('*')
-      .order('created_at', { ascending: true });
+  const { table } = getQuery(event); // 쿼리 파라미터에서 테이블 이름 가져오기
 
-    if (error) {
-      console.error('Error fetching users:', error);
-      return { error: 'Error fetching users' };
-    }
-    
-    return data;
-  } catch (error) {
-    console.error('Unexpected error:', error);
-    return { error: 'Unexpected error' };
+  // 테이블 이름이 유효한지 확인
+  if (!table || typeof table !== 'string') {
+    return { error: 'Invalid table name' };
   }
+
+  const { data, error } = await supabase
+    .from(table)
+    .select('*')
+    .order('created_at', { ascending: true });
+
+  if (error) {
+    console.error('Error fetching users:', error);
+    return { error: 'Error fetching users' };
+  }
+  return data;
 });
